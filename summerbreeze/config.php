61a3a45d7c7270ebe556f0476009f9c292adc3ea
DipGhosh <micronixsystem@gmail.com>
2020-07-18 22:24:53 +0530

    Add database port and try catch blok for config file

--- summerbreeze/config.php
+++ summerbreeze/config.php
@@ -1,12 +1,17 @@
 <?php
-include('../dbinfo.php');
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
 $opt = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
+
+try {
 $pdo = new PDO($dsn, $user, $pass, $opt);
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+
 
