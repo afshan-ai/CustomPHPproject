b85d00818e6780b59bb100c2291e5a5d9b27ccc9
DipGhosh <micronixsystem@gmail.com>
2020-07-21 18:47:39 +0530

    Add a new table dental_key for salt capturing.

--- cooksville/db.php
+++ cooksville/db.php
@@ -1,5 +1,16 @@
 <?php
-include('config.php');
+include('../dbinfo.php');
+
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
 $ongoing_query = "CREATE TABLE `dental1_key` (`id` int(11) NOT NULL, `salt` text NOT NULL ) ";
 
         $statement = $pdo->prepare($ongoing_query);
