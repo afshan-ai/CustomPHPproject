4b7332e457a9cdcc652803545d2e9adc5d940933
DipGhosh <micronixsystem@gmail.com>
2020-07-24 15:06:12 +0530

    Fix code isue for db insertion file

--- summerbreeze/db.php
+++ summerbreeze/db.php
@@ -1,5 +1,20 @@
 <?php
-include('../config.php');
+$host = 'appfordentists-db.cxypqkxdaqf2.ca-central-1.rds.amazonaws.com';
+$port = 3306;
+$db   = 'appfddb';
+$user = 'appfddbuser';
+$pass = '1Wtch0nUT3am';
+$charset = 'utf8';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+
  $ongoing_query = "CREATE TABLE `dentalsb_admin` (
   `id` int(11) NOT NULL,
   `username` varchar(250) NOT NULL,
